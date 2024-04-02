eb019a32c85056102c492b0e8bc8ea28ad6bb585
Ayan <ayan2572006@gmail.com>
2024-04-02 21:33:20 +0530

    all Product fetch done

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,10 +2,12 @@ import asyncHandler from "../middlewares/asyncHandler.js";
 import Product from "../models/productModel.js";
 import getDataUri from "../utils/dataUri.js";
 import cloudinary from "cloudinary"
+import ErrorHandler from "../utils/errorHandler.js";
+
 const createProduct = asyncHandler(async (req, res, next) => {
-  
   const file = req.file;
   const { name, description, price, category, quantity, brand } = req.body;
+  if(!name || !description || !price || !category || !quantity || !brand) return next(new ErrorHandler("Please Enter all fields",400))
 const fileUri = getDataUri(file)
 const mycloud = await cloudinary.v2.uploader.upload(fileUri.content)
   await Product.create({
@@ -25,5 +27,12 @@ const mycloud = await cloudinary.v2.uploader.upload(fileUri.content)
     message: "Product Created Successfully",
   });
 });
+const getAllProduct = asyncHandler(async (req, res, next) => {
+const products = await Product.find({})
+  res.status(201).json({
+    success: true,
+  products
+  });
+});
 
-export { createProduct };
+export { createProduct,getAllProduct };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { createProduct } from "../controllers/productController.js";
+import { createProduct,getAllProduct } from "../controllers/productController.js";
 import fileUpload from "../middlewares/multer.js";
 
 const router = express.Router()
 router.route("/new").post(fileUpload,createProduct)
+router.route("/all").get(getAllProduct)
 
 export default router;
