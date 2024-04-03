a5ba29a333f8b0ee4bc8854dee29d3e282336587
Ayan <ayan2572006@gmail.com>
2024-04-03 05:48:40 +0530

    getAllProduct done

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,6 +3,7 @@ import Product from "../models/productModel.js";
 import getDataUri from "../utils/dataUri.js";
 import cloudinary from "cloudinary"
 import ErrorHandler from "../utils/errorHandler.js";
+import { myCache } from "../app.js";
 
 const createProduct = asyncHandler(async (req, res, next) => {
   const file = req.file;
@@ -27,12 +28,60 @@ const mycloud = await cloudinary.v2.uploader.upload(fileUri.content)
     message: "Product Created Successfully",
   });
 });
-const getAllProduct = asyncHandler(async (req, res, next) => {
-const products = await Product.find({})
-  res.status(201).json({
-    success: true,
-  products
-  });
-});
 
-export { createProduct,getAllProduct };
+
+ const getAllProduct = asyncHandler(
+  async (req, res, next) => {
+const { search, sort, category, price } = req.query;
+const page = Number(req.query.page) || 1
+const limit = Number(process.env.PRODUCT_PER_PAGE) || 1;
+const skip = (page - 1) * limit;
+const baseQuery = {};
+if(search){
+  baseQuery.name={
+    $regex: search,
+    $options: "i",
+  }
+}
+if (price)
+baseQuery.price = {
+  $lte: Number(price),
+};
+
+if (category) baseQuery.category = category;
+const allProducts = await Product.find(baseQuery)
+.sort(sort && {price:sort === "asc" ? 1 : -1})
+.limit(limit)
+.skip(skip);
+
+const [products, filteredProduct] = await Promise.all([
+  allProducts,
+  Product.find(baseQuery),
+]);
+const totalPage = Math.ceil(filteredProduct.length / limit);
+    return res.status(200).json({
+      success: true,
+      products,
+      totalPage
+    });
+  }
+);
+
+const getProductById = asyncHandler(async(req,res,next)=>{
+  let product
+const id=req.params.id;
+if(myCache.has(`product-${id}`))product = JSON.parse(myCache.get(`product-${id}`))
+else{
+  product=await Product.findById(id)
+  myCache.set(`product-${id}`,JSON.stringify(product))
+}
+return res.status(200).json({
+  success: true,
+  product,
+});
+})
+
+const updateProduct = asyncHandler(async(req,res,next)=>{
+  
+})
+export { createProduct,getAllProduct,getProductById };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { createProduct,getAllProduct } from "../controllers/productController.js";
+import { createProduct,getAllProduct, getProductById } from "../controllers/productController.js";
 import fileUpload from "../middlewares/multer.js";
 
 const router = express.Router()
 router.route("/new").post(fileUpload,createProduct)
 router.route("/all").get(getAllProduct)
+router.route("/:id").get(getProductById)
 
 export default router;
