df5383933fe682b3a64c4b8d03d4d0268aefacd8
Ayan <ayan2572006@gmail.com>
2024-04-22 21:37:54 +0530

    update product fix

--- controllers/productController.js
+++ controllers/productController.js
@@ -85,40 +85,45 @@ const totalPage = Math.ceil(filteredProduct.length / limit);
 );
 
 const getProductById = asyncHandler(async(req,res,next)=>{
-  let product
+ 
 const id=req.params.id;
-if(myCache.has(`product-${id}`))product = JSON.parse(myCache.get(`product-${id}`))
-else{
-  product=await Product.findById(id)
-  myCache.set(`product-${id}`,JSON.stringify(product))
-}
+
+const product=await Product.findById(id)
+
 return res.status(200).json({
   success: true,
   product,
 });
 })
 
-const updateProduct = asyncHandler(async(req,res,next)=>{
-const id=req.params.id
-const {name, description, price, category, quantity, brand}=req.body
-const product = await Product.findById(id)
+const updateProduct = asyncHandler(async(req, res, next) => {
+  const { name, description, price, category, quantity, brand,stock } = req.body;
+  const id = req.params.id;
 
-if(!product) return next(new ErrorHandler("product not found",400))
+  const product = await Product.findById(id);
 
-if(name)product.name=name
-if(description)product.description=description
-if(price)product.price=price
-if(category)product.category=category
-if(quantity) product.quantity=quantity
-if(brand) product.brand=brand
-await product.save()
-await invalidateCache({product:true});
- res.status(200).json({
+  if (!product) return next(new ErrorHandler("Product not found", 400));
+
+  if (name) product.name = name;
+  if (description) product.description = description;
+  if (price) product.price = price;
+  if (category) product.category = category;
+  if (quantity) product.quantity = quantity;
+  if (stock) product.stock = stock;
+  if (brand) product.brand = brand;
+
+  await product.save();
+
+  // Invalidate cache after saving the updated product
+  await invalidateCache({ product: true });
+
+  res.status(200).json({
     success: true,
-    message: "product Update success",
+    message: "Product update success",
     product
   });
-})
+});
+
 const updateProductImage = asyncHandler(async(req,res,next)=>{
   const file = req.file;
   const id=req.params.id
@@ -252,22 +257,49 @@ const deleteReview = asyncHandler(async (req, res, next) => {
 });
 
 
-// Revalidate on New,Update,Delete Product & on New Order
- const getAdminProducts = asyncHandler(async (req, res, next) => {
 
-  let products;
-  if (myCache.has("all-products"))
-    products = JSON.parse(myCache.get("all-products"));
-  else {
-    products = await Product.find({});
-    myCache.set("all-products", JSON.stringify(products));
+
+
+
+
+
+
+const getAdminProducts = asyncHandler(
+  async (req, res, next) => {
+const { search, sort, category, price } = req.query;
+const page = Number(req.query.page) || 1
+const limit = Number(process.env.PRODUCT_PER_PAGE) || 1;
+const skip = (page - 1) * limit;
+const baseQuery = {};
+if(search){
+  baseQuery.name={
+    $regex: search,
+    $options: "i",
   }
+}
+if (price)
+baseQuery.price = {
+  $lte: Number(price),
+};
 
-  return res.status(200).json({
-    success: true,
-    products,
-  });
-});
+if (category) baseQuery.category = category;
+const allProducts = await Product.find(baseQuery)
+.sort(sort && {price:sort === "asc" ? 1 : -1})
+.limit(limit)
+.skip(skip);
+
+const [products, filteredProduct] = await Promise.all([
+  allProducts,
+  Product.find(baseQuery),
+]);
+const totalPage = Math.ceil(filteredProduct.length / limit);
+    return res.status(200).json({
+      success: true,
+      products,
+      totalPage
+    });
+  }
+);
 
 const getTopProducts = asyncHandler(async(req,res,next)=>{
   const topProduct = await Product.find({}).sort({rating: -1}).limit(6);
