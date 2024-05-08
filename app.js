0306f73363d74c74771a1f7ec8d650601b09d0f2
Ayan <ayan2572006@gmail.com>
2024-05-08 21:22:24 +0530

    fix

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
