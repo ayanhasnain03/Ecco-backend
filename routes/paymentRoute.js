216c2ad197f23938d31e28e870c281b635d9d5ae
Ayan <ayan2572006@gmail.com>
2024-05-18 23:44:35 +0530

    fix

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -2,7 +2,7 @@ import express from "express"
 import { applyDiscount, createPayment, newCoupon,getAllCoupan, deleteCoupon } from "../controllers/paymentController.js"
 import { authorizeAdmin, isAuthenticatedUser } from "../middlewares/auth.js"
 const router = express.Router()
-router.route("/create").post(isAuthenticatedUser,createPayment)
+router.route("/create").post(createPayment)
 router.route("/coupon/create").post(isAuthenticatedUser,authorizeAdmin,newCoupon)
 router.route("/coupon/discount").get(isAuthenticatedUser,applyDiscount)
 router.route("/coupon/all").get(isAuthenticatedUser,authorizeAdmin,getAllCoupan)
